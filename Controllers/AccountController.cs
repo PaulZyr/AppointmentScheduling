9fc94a1a65574be14d63dd4af28a450cd2e8e6f0
paulzyr <pavel.ziryanov@ithoot.com>
2021-08-22 06:38:20 +0300

    add dbContext

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppointmentScheduling.Data;
+
+namespace AppointmentScheduling.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public AccountController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
